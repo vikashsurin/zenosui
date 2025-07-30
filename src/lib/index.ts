b260a448fede20eb64052d3810e1b6831fe8b4ef
VikashSurin <vikashsurin10@gmail.com>
2025-07-30 16:13:36 +0530

    updates extension from js to ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,9 @@
-export * from './ui/index.js';
+export * from './ui/index.ts';
 
-export * from './style/index.js';
+export * from './style/index.ts';
 
-export * from './utils/index.js';
+export * from './utils/index.ts';
 
-export * from './store/index.svelte.js';
+export * from './store/index.svelte.ts';
 
-export * from './types/index.js';
+export * from './types/index.ts';
