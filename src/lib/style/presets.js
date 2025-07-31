70b25ac63dba64cf861703999538253ba6acdf43
VikashSurin <vikashsurin10@gmail.com>
2025-07-31 11:34:07 +0530

    updated style

--- src/lib/style/presets.js
+++ src/lib/style/presets.js
@@ -1,16 +1,16 @@
 export const SIZE_PRESET = {
-	xs: 'text-xs px-2 min-h-6 gap-0.75',
-	sm: 'text-sm px-3 min-h-8 gap-1',
-	md: 'text-base px-4 min-h-10 gap-1.5',
-	lg: 'text-lg px-5 min-h-12 gap-2',
-	xl: 'text-xl px-6 min-h-14 gap-2.5',
-	'2xl': 'text-2xl px-7 min-h-16 gap-3',
-	'3xl': 'text-3xl px-8 min-h-20 gap-3.5',
-	'4xl': 'text-4xl px-9 min-h-24 gap-4',
-	'5xl': 'text-5xl px-10 min-h-28 gap-4.5',
-	'6xl': 'text-6xl px-11 min-h-32 gap-5',
-	'7xl': 'text-7xl px-12 min-h-36 gap-5.5',
-	'8xl': 'text-8xl px-13 min-h-40 gap-6'
+	xs: 'text-xs px-2 h-6 gap-0.75',
+	sm: 'text-sm px-3 h-8 gap-1',
+	md: 'text-base px-4 h-10 gap-1.5',
+	lg: 'text-lg px-5 h-12 gap-2',
+	xl: 'text-xl px-6 h-14 gap-2.5',
+	'2xl': 'text-2xl px-7 h-16 gap-3',
+	'3xl': 'text-3xl px-8 h-20 gap-3.5',
+	'4xl': 'text-4xl px-9 h-24 gap-4',
+	'5xl': 'text-5xl px-10 h-28 gap-4.5',
+	'6xl': 'text-6xl px-11 h-32 gap-5',
+	'7xl': 'text-7xl px-12 h-36 gap-5.5',
+	'8xl': 'text-8xl px-13 h-40 gap-6'
 };
 export const COLOR_PRESET = {
 	none: '',
