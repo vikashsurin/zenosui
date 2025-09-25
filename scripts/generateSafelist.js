1c5322dd77ba60bd498ea1e244d379c3f6e159d1
VikashSurin <vikashsurin10@gmail.com>
2025-09-25 16:57:10 +0530

    updated scripts

--- scripts/generateSafelist.js
+++ scripts/generateSafelist.js
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
-import * as all from '../lib/style/index.js';
+import * as style from '../src/lib/style/index.js';
 
 /**
  * @param {{ [s: string]: any; } | ArrayLike<any>} obj
@@ -18,30 +18,31 @@ function extractClasses(obj) {
 	return classes;
 }
 const safelist = [
-	...extractClasses(all.ICON_SIZE),
-	...extractClasses(all.SIZE_FOR_SWITCH),
-	...extractClasses(all.TEXT_SIZE),
-	...extractClasses(all.SIZE_PRESET),
-	...extractClasses(all.COLOR_PRESET),
-	...extractClasses(all.FILL_VARIANT),
-	...extractClasses(all.ROUNDED),
-	...extractClasses(all.PADDING),
-	...extractClasses(all.BACKGROUND_COLOR),
-	...extractClasses(all.CONTENT_COLOR),
-	...extractClasses(all.BORDER_COLOR), ,
-	...extractClasses(all.BORDER),
-	...extractClasses(all.OUTLINE_COLOR),
-	...extractClasses(all.TEXT_SIZE_WITH_HEIGHT),
-	...extractClasses(all.TEXT_SIZE_WITH_PADDING),
-	...extractClasses(all.ICON_PLACEHOLDER_SIZE),
-	...extractClasses(all.POSITION)
+	...extractClasses(style.ICON_SIZE),
+	...extractClasses(style.SIZE_FOR_SWITCH),
+	...extractClasses(style.TEXT_SIZE),
+	...extractClasses(style.SIZE_PRESET),
+	...extractClasses(style.COLOR_PRESET),
+	...extractClasses(style.FILL_VARIANT),
+	...extractClasses(style.ROUNDED),
+	...extractClasses(style.PADDING),
+	...extractClasses(style.BACKGROUND_COLOR),
+	...extractClasses(style.CONTENT_COLOR),
+	...extractClasses(style.BORDER_COLOR), ,
+	...extractClasses(style.BORDER),
+	...extractClasses(style.OUTLINE_COLOR),
+	...extractClasses(style.TEXT_SIZE_WITH_HEIGHT),
+	...extractClasses(style.TEXT_SIZE_WITH_PADDING),
+	...extractClasses(style.ICON_PLACEHOLDER_SIZE),
+	...extractClasses(style.POSITION)
 
 ];
 console.log('Extracted classes:', safelist);
+
 async function generateSafelist() {
 	const content = JSON.stringify(safelist, null, 2);
 	console.log('Generated safelist content:', content);
-	const filePath = path.join(process.cwd(), 'dist', 'safelist.json');
+	const filePath = path.join(process.cwd(), 'src/lib', 'safelist.json');
 
 	await fs.mkdir(path.dirname(filePath), { recursive: true });
 	await fs.writeFile(filePath, content);
