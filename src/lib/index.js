33055fda6d7ac2df0d63d11abc74baf65553528e
VikashSurin <vikashsurin10@gmail.com>
2025-09-25 15:03:35 +0530

    updated file extension

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 export * from './ui/index.js';
 export * from './style/index.js';
 export * from './utils/index.js';
-export * from './stores/index.svelte.ts';
+export * from './stores/index..js';
 export { safelist as tailwindSafelist } from './tailwind.js';
