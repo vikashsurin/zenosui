30c655506f6c90020739fa5739df306267981e31
VikashSurin <vikashsurin10@gmail.com>
2025-10-02 19:05:42 +0530

    added DocHeader.svelte

---
+++ src/lib/internal/DocHeader.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { title, description } = $props();
+</script>
+
+<div class="p-4">
+	<h1 class="text-3xl font-bold">{title}</h1>
+	<p class="py-4">{description}</p>
+</div>
--- src/routes/docs/accordion/+page.svelte
+++ src/routes/docs/accordion/+page.svelte
@@ -2,6 +2,12 @@
 	import AccordionDemo from './AccordionDemo.svelte';
 	import source from './AccordionDemo.svelte?raw';
 	import PreviewCode from '$lib/internal/PreviewCode.svelte';
+	import DocHeader from '$lib/internal/DocHeader.svelte';
 </script>
+
+<DocHeader
+	title="Accordion"
+	description="Accordion is a component that is used to display a list of items that can be expanded or collapsed."
+/>
 
 <PreviewCode {source} Component={AccordionDemo} />
