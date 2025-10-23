0da00f7ff161687662a482308cbf45f64e445664
VikashSurin <vikashsurin10@gmail.com>
2025-10-23 14:34:37 +0530

    updated config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		adapter: adapter({
+			runtime: 'nodejs22.x'
+		})
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
