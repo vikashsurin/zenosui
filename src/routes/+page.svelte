c447ef347f035ab4d804d514942a8afe1504ed47
VikashSurin <vikashsurin10@gmail.com>
2025-11-02 12:27:47 +0530

    updated docs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Cog from '@lucide/svelte/icons/cog';
+</script>
+
+<div class="flex h-screen w-screen items-center justify-center gap-2 bg-amber-100 px-8 py-4">
+	<span class="animate-[spin_3s_linear_infinite]">
+		<Cog />
+	</span>
+	<p class="  text-xl">Website under development</p>
+</div>
